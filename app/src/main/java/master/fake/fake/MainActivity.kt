ed49a1c8555b2a4a393cd48bab7ed8d64138eea7
Mohamed Halawa <thisishalawa@gmail.com>
2021-08-19 14:33:26 +0200

    setup Observing LiveData from the QuotesViewModel

--- app/src/main/java/master/fake/fake/MainActivity.kt
+++ app/src/main/java/master/fake/fake/MainActivity.kt
@@ -2,17 +2,38 @@ package master.fake.fake
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import master.fake.fake.databinding.ActivityMainBinding
+
+const val TAG = "_tag_debug"
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+/*
+        private lateinit var navController: NavController
+        navController = Navigation.findNavController(this, R.id.hostFragment)
+        bottom_nav.setupWithNavController(navController)
+*/
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        binding.bottomNav.setupWithNavController(navController)
 
 
     }
--- app/src/main/java/master/fake/fake/dao_practice/quote/repository/QuoteRepository.kt
+++ app/src/main/java/master/fake/fake/dao_practice/quote/repository/QuoteRepository.kt
@@ -6,21 +6,16 @@ import master.fake.fake.dao_practice.quote.data.Quote
 /*
 * Repository as a point of control
 Repositories are classes which do all of the decision making regarding app’s data.
-*  Should you fetch new data from the server or is it enough to use the local data?
-* Do you need to keep 5 days of weather data localy
-*  or only 3 days? Making such decisions is the job of a repository.
-I know, having a repository class in this particular project may seem redundant
-* – you only have a single DAO and you don’t even have a backend. While this is true,
-* this does not stop you from learning about the core work repositories have to do –
-*  being the single source of truth for all the data which ViewModels request.
-Again, it does not make sense to have multiple repository objects, so it will be a singleton.
-*  This time you need to pass in the FakeQuoteDao for repository to fulfill its role.
-*  You will use dependency injection to supply this FakeQuoteDao instance to the repository.
+
+*
+* ,it does not make sense to have multiple repository objects, so it will be a singleton.
+*  This time we need to pass in the FakeQuoteDao for repository to fulfill its role.
+*  will use dependency injection to supply this FakeQuoteDao instance to the repository.
 *
 * */
 // FakeQuoteDao must be passed in - it is a dependency
-// You could also instantiate the DAO right inside the class without all the fuss, right?
-// No. This would break testability - you need to be able to pass a mock version of a DAO
+//  could also instantiate the DAO right inside the class without all the fuss?
+// Nope . This would break testability - you need to be able to pass a mock version of a DAO
 // to the repository (e.g. one that upon calling getQuotes() returns a dummy list of quotes for testing)
 // This is the core idea behind DEPENDENCY INJECTION - making things completely modular and independent.
 
